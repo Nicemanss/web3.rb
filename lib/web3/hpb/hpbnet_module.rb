6d54ef8e9e151622e8294030f63654ab95e8512f
Nicemanss <43435071+Nicemanss@users.noreply.github.com>
2019-09-10 15:39:04 +0200

    Update and rename net_module.rb to hpbnet_module.rb

---
+++ lib/web3/hpb/hpbnet_module.rb
@@ -0,0 +1,31 @@
+module Web3
+  module HpbNet
+
+    class HpbNetModule
+
+      PREFIX = 'net_'
+
+      def initialize web3_rpc
+        @web3_rpc = web3_rpc
+      end
+
+      def version
+        from_hex @web3_rpc.request("#{PREFIX}#{__method__}")
+      end
+
+      def listening
+        from_hex @web3_rpc.request("#{PREFIX}#{__method__}")
+      end
+
+      def peerCount
+        from_hex @web3_rpc.request("#{PREFIX}#{__method__}")
+      end
+
+      def method_missing m, *args
+        @web3_rpc.request "#{PREFIX}#{m}", args[0]
+      end
+
+
+    end
+  end
+end
